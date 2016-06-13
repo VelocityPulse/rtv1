e637bd3d01d71729120cf4315646e9ea02ad6ac2
Clement CHAMEYRAT <cchameyr@e3r10p2.42.fr>
2016-06-13 16:05:08 +0200

    mise a jour de la libft

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: cchameyr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/21 12:20:08 by cchameyr          #+#    #+#             */
-/*   Updated: 2016/06/01 12:14:30 by cchameyr         ###   ########.fr       */
+/*   Updated: 2016/06/13 16:03:05 by cchameyr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,13 @@ typedef struct	s_ptd
 	double			x;
 	double			y;
 }				t_ptd;
+
+typedef struct	s_ptd3d
+{
+	double			x;
+	double			y;
+	double			z;
+}				t_ptd3d;
 
 typedef struct	s_ptll
 {
@@ -129,12 +136,14 @@ int				ft_memlen(void **tab);
 
 t_pt			ft_make_pt(int x, int y);
 t_ptd			ft_make_ptd(double x, double y);
+t_ptd3d			ft_make_ptd3d(double x, double y, double z);
 t_ptll			ft_make_ptll(long long x, long long y);
 t_pt3d			ft_make_pt3d(int x, int y, int z);
 t_pt3df			ft_make_pt3df(float x, float y, float z);
 
 t_pt			ft_add_pt(t_pt pt1, t_pt pt2);
 t_ptd			ft_add_ptd(t_ptd pt1, t_ptd pt2);
+t_ptd3d			ft_add_ptd3d(t_ptd3d pt1, t_ptd3d pt2);
 t_ptll			ft_add_ptll(t_ptll pt1, t_ptll pt2);
 t_pt3d			ft_add_pt3d(t_pt3d pt1, t_pt3d pt2);
 t_pt3df			ft_add_pt3df(t_pt3df pt1, t_pt3df pt2);
