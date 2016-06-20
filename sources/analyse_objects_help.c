38a724d9f2fc8bfe2774ab36c60d56d24722d99d
Clement CHAMEYRAT <velocitypulse@outlook.fr>
2016-06-20 11:38:40 +0200

    suppression du debug

--- sources/analyse_objects_help.c
+++ sources/analyse_objects_help.c
@@ -6,7 +6,7 @@
 /*   By: cchameyr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/19 12:16:13 by cchameyr          #+#    #+#             */
-/*   Updated: 2016/06/19 16:02:46 by cchameyr         ###   ########.fr       */
+/*   Updated: 2016/06/20 11:38:23 by cchameyr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,6 @@
 
 int		analyse_sphere_help(char **str, t_sphere *sphere)
 {
-
-	printf("\n\nTEST ATOI :\n%f\n\n", ft_atoid("3.43  "));
 	if (ft_memlen((void **)str) != 4 ||
 			!analyse_3d_value_d(str[0], &sphere->x, &sphere->y,
 			&sphere->z) ||
